26715c001e817011aee304703a27967230c9cda6
Joao Victor <jvcoutoo@gmail.com>
2022-04-24 12:18:50 -0300

    eslint fixes and prettier

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -14,7 +14,7 @@ interface MyAppProps extends AppProps {
   emotionCache?: EmotionCache;
 }
 
-export default function MyApp(props: MyAppProps) {
+export default function MyApp(props: MyAppProps){
   const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
   return (
     <CacheProvider value={emotionCache}>
