a9a711866379b0c672989067d87e9fd948bf36a0
Joao Victor <jvcoutoo@gmail.com>
2022-04-24 12:58:49 -0300

    set teacher to dataset

--- server/src/database/dataset/Data.sql
+++ server/src/database/dataset/Data.sql
@@ -1,5 +1,9 @@
-INSERT INTO course (name) VALUES 
-('Ciência da Computação'),
-('Engenharia Mecânica'),
-('Administração'),
-('Economia');
+INSERT INTO course (name, created_at) VALUES 
+('Ciência da Computação', '2022-04-23 21:11:54.649'),
+('Engenharia Mecânica', '2022-04-23 21:11:54.649'),
+('Administração', '2022-04-23 21:11:54.649'),
+('Economia', '2022-04-23 21:11:54.649');
+
+
+INSERT INTO teacher (name, email, course_id, password, created_at) VALUES
+('Joao Victor', 'joaovictor@ufsj.edu.br', 1, '$2a$10$rlvRqgwNTH.3uxTkxr0MNuaG82Ik0JdeURd26RItFdJZlbfBsk0iK', '2022-04-23 21:11:54.649');
