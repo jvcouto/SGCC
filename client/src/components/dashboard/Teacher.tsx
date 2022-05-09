e7124fbbca694d180f60f7ff6af8d37784d3a3b8
Joao Victor <jvcoutoo@gmail.com>
2022-05-08 21:02:10 -0300

    show user values on dashboard page

--- client/src/components/dashboard/Teacher.tsx
+++ client/src/components/dashboard/Teacher.tsx
@@ -3,7 +3,11 @@ import { useAuth } from "../../contexts/AuthContext";
 
 function TeacherDeshboard() {
   const { user } = useAuth();
-  return <h1>{user?.name}</h1>;
+  return (
+    <h1>
+      Bem vindo ao Dashboard de {user?.role}, {user?.name}!!!
+    </h1>
+  );
 }
 
 export default TeacherDeshboard;
