378a4dc129cf2805531dc9ecf228a43dad10c34c
João Victor Couto <jvcoutoo@gmail.com>
2024-05-04 11:17:39 -0300

    move db config

--- server/dist/config/database.js
+++ server/dist/config/database.js
@@ -5,9 +5,24 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const typeorm_1 = require("typeorm");
 const logger_1 = __importDefault(require("../utils/logger"));
+const config = {
+    type: "postgres",
+    host: process.env.DB_HOST ?? "localhost",
+    port: process.env.DB_PORT ?? 5432,
+    username: process.env.DB_USER ?? "postgres",
+    password: process.env.DB_PASSWORD ?? "postgres",
+    database: process.env.DB_DATABASE ?? "postgres",
+    entities: ["src/models/*.ts"],
+    migrations: ["src/database/migrations/*.ts"],
+    synchronize: true,
+    cli: {
+        migrationsDir: "src/database/migrations",
+        entitiesDir: "src/models",
+    },
+};
 class Database {
     static async createConnection() {
-        const connection = await (0, typeorm_1.createConnection)();
+        const connection = await (0, typeorm_1.createConnection)(config);
         logger_1.default.info("TypeORM initialized!!");
         return connection;
     }
--- server/src/config/database.ts
+++ server/src/config/database.ts
@@ -1,9 +1,25 @@
-import { createConnection } from "typeorm";
+import { ConnectionOptions, createConnection } from "typeorm";
 import Logger from "@utils/logger";
+
+const config = {
+  type: "postgres",
+  host: process.env.DB_HOST ?? "localhost",
+  port: process.env.DB_PORT ?? 5432,
+  username: process.env.DB_USER ?? "postgres",
+  password: process.env.DB_PASSWORD ?? "postgres",
+  database: process.env.DB_DATABASE ?? "postgres",
+  entities: ["src/models/*.ts"],
+  migrations: ["src/database/migrations/*.ts"],
+  synchronize: true,
+  cli: {
+    migrationsDir: "src/database/migrations",
+    entitiesDir: "src/models",
+  },
+} as ConnectionOptions;
 
 class Database {
   static async createConnection() {
-    const connection = await createConnection();
+    const connection = await createConnection(config);
     Logger.info("TypeORM initialized!!");
     return connection;
   }
