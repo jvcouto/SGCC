b1ce04acee129339d43f876970efdcb718b3e462
João Victor Couto <jvcoutoo@gmail.com>
2024-05-04 09:49:19 -0300

    fix: db host env variable

--- server/ormconfig.js
+++ server/ormconfig.js
@@ -1,6 +1,6 @@
 module.exports = {
     type: "postgres",
-    host: "localhost",
+    host: process.env.DB_HOST ?? "localhost",
     port:  process.env.DB_PORT ?? 5432,
     username: process.env.DB_USER ?? "postgres",
     password:  process.env.DB_PASSWORD ?? "postgres",
