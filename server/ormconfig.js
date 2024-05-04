e60d263658ecee0c6d42b48d3384c9f77d582a0a
João Victor Couto <jvcoutoo@gmail.com>
2024-05-04 13:21:57 -0300

    fix: entities prod

--- server/ormconfig.js
+++ server/ormconfig.js
@@ -1,3 +1,5 @@
+
+
 module.exports = {
     type: "postgres",
     host: process.env.DB_HOST ?? "localhost",
@@ -5,7 +7,7 @@ module.exports = {
     username: process.env.DB_USER ?? "postgres",
     password:  process.env.DB_PASSWORD ?? "postgres",
     database:  process.env.DB_DATABASE ?? "postgres",
-    entities: ["src/models/*.ts", "dist/models/*.js", "models/*.js", `${__dirname}/models/*.js`],
+    entities: process.env.NODE_ENV === 'PROD' ? [`dist/models/*.js`] : ["src/models/*.ts"],
     migrations: ["src/database/migrations/*.ts"],
     synchronize: true,
     cli: {
